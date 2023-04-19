c5e0ce277b85b8c7b61e635c55d7de04dea31927
Gabor Sinka <hessor@gmail.com>
2023-04-19 17:44:58 +0200

    create words model, and initial seeder

---
+++ database/seeders/WordSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class WordSeeder extends Seeder
+{
+    private $initialWords = [
+        'reward',
+        'shiver',
+        'regret',
+        'carbon',
+        'island',
+        'impound',
+        'extreme',
+        'inspire',
+        'finance',
+        'control',
+        'collapse',
+        'medicine',
+        'frighten',
+        'observer',
+        'classify',
+        'monstrous',
+        'orchestra',
+        'executive',
+        'fireplace',
+        'essential',
+        'prevalence',
+        'background',
+        'particular',
+        'attraction',
+        'pedestrian',
+        'unfortunate',
+        'charismatic',
+        'institution',
+        'destruction',
+        'presentation',
+        'manufacturer',
+        'interference',
+        'announcement',
+        'presidential',
+        'inappropriate',
+        'embarrassment',
+        'consideration',
+        'comprehensive',
+        'communication',
+        'representative',
+        'responsibility',
+        'constitutional',
+        'discrimination',
+        'recommendation',
+    ];
+
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        foreach ($this->initialWords as $word) {
+            \App\Models\Word::create(['value' => $word]);
+        }
+    }
+}
